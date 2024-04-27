8661502f96775e085c7bf30b5a33b62b167d9545
noah <nattapong.w1004@gmail.com>
2024-04-28 00:00:18 +0700

    mqtt, update sendAlarm() and add onConnect onDisconnect

--- src/mqtt_iotalarm.cpp
+++ src/mqtt_iotalarm.cpp
@@ -6,14 +6,26 @@ AsyncMqttClient mqttClient;
 
 
 
+void onMqttConnect(bool sessionPresent) {
+    debugln("MQTT connected successfully!");
+    debug("Session present: ");
+    debugln(sessionPresent);
+}
+
+void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
+    debugln("Disconnected from MQTT.");
+}
+
 void mqtt_init (deviceConfigType *s_config) {
     // Set broker URL, port etc.
-    // mqttClient.setCredentials(s_config->mqttUser, s_config->mqttPassword);
-    mqttClient.setServer(s_config->mqttBrokerUrl, atoi(s_config->mqttPort));
     debug("set MQTT broker to ");
     debug(s_config->mqttBrokerUrl);
     debug(", port=");
     debugln(s_config->mqttPort);
+    // mqttClient.setCredentials(s_config->mqttUser, s_config->mqttPassword);
+    mqttClient.setServer(s_config->mqttBrokerUrl, s_config->mqttPort);
+    mqttClient.onConnect(onMqttConnect);
+    mqttClient.onDisconnect(onMqttDisconnect);
 }
 
 void mqtt_sendDeviceState (deviceConfigType *s_config, deviceStateType *s_state) {
@@ -37,20 +49,21 @@ void mqtt_sendDeviceState (deviceConfigType *s_config, deviceStateType *s_state)
     }
 }
 
-void mqtt_sendAlarm (deviceConfigType *s_config, deviceStateType *s_state, int type) {
-    if (s_state->wifiState == WIFI_CONNECTED) {
-        // MQTT connect
-        //
-        // MQTT publish
-        //
+void mqtt_sendAlarm (deviceConfigType *s_config, int type) {
+    if (type == ALARM_HELP) {
+        debugln("Sending HELP");
+        mqttClient.publish(s_config->mqttTopicHelp, 0, false, "1");
+    }
+    else if (type == ALARM_SECURITY) {
+        debugln("Sending SECURITY");
+        mqttClient.publish(s_config->mqttTopicSecurity, 0, false, "2");
     }
 }
  
-void mqtt_connect (deviceConfigType *s_config, deviceStateType *s_state) {
-    if (s_state->wifiState == WIFI_CONNECTED) {
-        // send MQTT connect to broker
-        debugln("Connecting to MQTT broker");
-        mqttClient.connect();
-    }
+void mqtt_connect (deviceConfigType *s_config) {
+    // send MQTT connect to broker
+    debugln("Connecting to MQTT broker");
+    mqttClient.connect();
 }
+
 
