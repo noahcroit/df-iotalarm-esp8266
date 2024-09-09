039be73c34a3d4ceb47648c8d8cecac3c501c832
noah <nattapong.w1004@gmail.com>
2024-09-10 01:48:35 +0700

    timestamp_iotalarm : create date extraction

--- src/timestamp_iotalarm.cpp
+++ src/timestamp_iotalarm.cpp
@@ -26,9 +26,20 @@ void timestamp_start(deviceStateType *s_state) {
 }
 
 void timestamp_updateRequest(deviceStateType *s_state) {
+    struct tm *ptm;
+    String currentDate;
+    int currentYear;
+    int currentMonth;
+    int monthDay;
     if (s_state->wifiState == WIFI_CONNECTED) {
         timeClient.update();
         s_state->timestamp = timeClient.getFormattedTime();
         s_state->timestampEpoch = timeClient.getEpochTime();
+
+        ptm = gmtime ((time_t *)&(s_state->timestampEpoch));
+        currentYear = ptm->tm_year+1900;
+        currentMonth = ptm->tm_mon+1;
+        monthDay = ptm->tm_mday;
+        currentDate = String(currentYear) + "-" + String(currentMonth) + "-" + String(monthDay);
     }
 }
